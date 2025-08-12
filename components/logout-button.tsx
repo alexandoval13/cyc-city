54244186f18d86188968dd2976e837d7a226aed7
Alexandra Sandoval <alexandoval13@gmail.com>
2025-08-12 16:44:25 -0700

    Update user context and clear user after logout

--- components/logout-button.tsx
+++ components/logout-button.tsx
@@ -3,13 +3,20 @@
 import { createClient } from '@/utils/supabase/client';
 import { Button } from '@/components/ui/button';
 import { useRouter } from 'next/navigation';
+import { UserContext } from '@/app/contexts/UserContext';
+import { useContext } from 'react';
 
 export function LogoutButton() {
   const router = useRouter();
 
+  const { user, clearUser } = useContext(UserContext);
+
   const logout = async () => {
     const supabase = createClient();
     await supabase.auth.signOut();
+
+    if (user && clearUser) clearUser();
+
     router.push('/auth/login');
   };
 
