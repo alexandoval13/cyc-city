a4d4dea48f244d8020583ca08a8c559f9f8e097f
Alexandra Sandoval <alexandoval13@gmail.com>
2025-08-12 13:42:31 -0700

    Delets unused files

--- app/api/validate-qr.ts
+++
@@ -1,6 +0,0 @@
-// QR validation functions
-// transfer ownership, service history (dual validation) -> only the owner can start this route
-
-// get service history -> anyone can do this
-
-// get bike details -> anyone can do this ()
