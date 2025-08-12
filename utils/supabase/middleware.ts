b63be44f6f4cfae4a5df5411733edd7fac3416f8
Alexandra Sandoval <alexandoval13@gmail.com>
2025-08-12 16:43:23 -0700

    Redirect to home if user exists and at /auth

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -52,6 +52,17 @@ export async function updateSession(request: NextRequest) {
     return NextResponse.redirect(url);
   }
 
+  // redirect to home if user is already logged in
+  if (
+    user &&
+    (request.nextUrl.pathname.startsWith('/auth') ||
+      request.nextUrl.pathname.startsWith('/login'))
+  ) {
+    const url = request.nextUrl.clone();
+    url.pathname = '/';
+    return NextResponse.redirect(url);
+  }
+
   // IMPORTANT: You *must* return the supabaseResponse object as it is.
   // If you're creating a new response object with NextResponse.next() make sure to:
   // 1. Pass the request in it, like so:
