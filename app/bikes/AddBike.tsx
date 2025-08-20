3b7cdc4a7f85abdf91e076a8a71841c23634dc7b
Alexandra Sandoval <alexandoval13@gmail.com>
2025-08-19 17:52:02 -0700

    Use shadcn dialog

--- app/bikes/AddBike.tsx
+++ app/bikes/AddBike.tsx
@@ -2,29 +2,27 @@
 import AddBikeForm from '@/components/forms/AddBikeForm';
 import { AddIcon } from '@/components/icons/add';
 import { Button } from '@/components/ui/button';
-import { useState } from 'react';
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTrigger,
+} from '@/components/ui/dialog';
 
 export default function AddBike() {
-  const [formOpen, setFormOpen] = useState<boolean>(false);
-
-  const handleClose = () => {
-    setFormOpen(false);
-  };
-
-  // todo: fix bg color with scheme
   return (
-    <main className="bg-inherit">
-      <Button className="rounded-full" onClick={() => setFormOpen(true)}>
-        <AddIcon />
-      </Button>
-      {formOpen ? (
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-1001 w-full h-full bg-gray-600">
-          <div className="fixed top-2 right-2">
-            <Button onClick={handleClose}>X</Button>
-          </div>
-          <AddBikeForm />
-        </div>
-      ) : null}
-    </main>
+    <Dialog>
+      <DialogTrigger>
+        <Button className="rounded-full">
+          <AddIcon />
+        </Button>
+      </DialogTrigger>
+      <DialogHeader />
+      <DialogContent className="h-full md:h-auto md:max-h-[360px] max-w-screen p-0 overflow-auto">
+        <AddBikeForm />
+      </DialogContent>
+      <DialogFooter />
+    </Dialog>
   );
 }
